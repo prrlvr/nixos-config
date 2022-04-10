484390730091c0c46952900d6e8e0279018c24c0
Pierre-Olivier Rey <pierre-olivier.rey@epita.fr>
2022-04-10 17:55:14 +0200

    machines: shinji (lab worker)

--- machines/shinji.nix
+++ machines/shinji.nix
@@ -4,14 +4,13 @@
   imports = [
     # Machine own config
     ./shinji
-    ./shinji/users.nix
     ./shinji/hardware.nix
-    ./shinji/home.nix
 
     # Inherit default config
     ./default/crypt-system.nix
     ./default/locales.nix
     ./default/nix.nix
+    ./default/users.nix
     ./default/programs.nix
     ./default/xserver.nix
   ];
